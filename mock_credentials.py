56fd9c4a49660d40e6f97282284e980e3e121725
thomatou <tc408@ic.ac.uk>
2020-11-21 12:25:27 -0800

    Added script and example credentials file

---
+++ mock_credentials.py
@@ -0,0 +1,16 @@
+# type('month'): int
+# type('day'): int
+# type('email'): string
+# type('password'): string
+
+
+
+booking =   {
+0: {
+'month': 12,
+'day' : 21,
+'email' : 'your_email',
+'password' : 'your_password',
+'resort' : 'Crystal Mountain Resort',
+}
+
