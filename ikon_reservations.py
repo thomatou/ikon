375e7e4549abee9ac88ec174743e7b3224b0a06e
thomatou <tc408@ic.ac.uk>
2020-11-21 20:44:54 -0800

    Fleshed out the main code

--- ikon_reservations.py
+++ ikon_reservations.py
@@ -1,11 +1,9 @@
 import time
-from datetime import datetime
+from datetime import datetime, timedelta
 from pytz import timezone
 import traceback
 import schedule
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.support.ui import Select
 import credentials
 
 pst = timezone('America/Los_Angeles')
@@ -17,6 +15,11 @@ class Automate_reservation:
         Creates the class variables required to book a slot.
         Contained in the credentials.py file.
         """
+        self.date = datetime(booking_details['year'],
+                            booking_details['month'],
+                            booking_details['day'])
+
+        self.year = booking_details['year']
         self.month = booking_details['month']
         self.day = booking_details['day']
         self.email = booking_details['email']
@@ -37,30 +40,25 @@ class Automate_reservation:
         # let's cancel the job. Can probably make that dependent on the exact
         # time of booking.
         # The server runs in UTC time so have to convert to pacific time first
-        #pst = timezone('America/Los_Angeles')
+        pst = timezone('America/Los_Angeles')
 
-        if datetime.now().astimezone(pst) > datetime(2020, self.month, self.day + 1).astimezone(pst):
-            print('doing schedule.CancelJob')
+        if datetime.now().astimezone(pst) > self.date.astimezone(pst) + \
+                                            timedelta(days=1):
+            print("Returning schedule.CancelJob, since we're past the date of the desired reservation.")
             return schedule.CancelJob
 
         browser = self.new_browser_instance(test_mode=test_mode)
-        #
-        # time.sleep(5)
-        # print("we have a new browser instance!")
+
+        time.sleep(5)
 
         new_browser = self.login(browser)
-        # print("we've logged in")
-        # time.sleep(5)
-
-        time.sleep(1)
 
         browser = self.find_slot(new_browser)
 
-        # time.sleep(5)
-        #
-        # if browser:
-        #     print('returning canceljob')
-        #     return self.book_slot(browser, test_mode=test_mode)
+        if browser:
+            print('returning canceljob')
+            return self.book_slot(browser, test_mode=test_mode)
+
 
     def new_browser_instance(self, test_mode=True):
         """
@@ -68,16 +66,15 @@ class Automate_reservation:
         when not in test mode (uses less resources).
         """
 
-        options = Options()
+        options = webdriver.ChromeOptions()
+
         if test_mode:
             options.headless = False
         else:
             options.headless = True
 
-        browser = webdriver.Firefox(
-                                    options=options,
-                                    executable_path='/Users/tchavas/geckodriver'
-                                    )
+        browser = webdriver.Chrome(executable_path='/Users/tchavas/chromedriver')
+
         return browser
 
     def login(self, browser):
@@ -90,15 +87,14 @@ class Automate_reservation:
         """
 
         # Log in address
-        browser.get(
-        'https://account.ikonpass.com/en/myaccount/add-reservations/'
-                    )
+        browser.get('https://account.ikonpass.com/en/login')
 
-        # # Fill in your details
-        browser.find_element_by_xpath(
-        '//*[@id="email"]').send_keys(self.email)
-        browser.find_element_by_xpath(
-        '//*[@id="sign-in-password"]').send_keys(self.password)
+        # Enter your login details
+        browser.find_element_by_xpath('//*[@id="email"]').send_keys(
+                            self.email)
+
+        browser.find_element_by_xpath('//*[@id="sign-in-password"]').send_keys(
+                            self.password)
 
         time.sleep(1)
 
@@ -115,138 +111,89 @@ class Automate_reservation:
         Only supports booking in the current month, or the following one.
         """
 
+        # Once you're logged in, go to reservations page
         browser.get(
-        'https://account.ikonpass.com/en/myaccount/add-reservations/'
+                'https://account.ikonpass.com/en/myaccount/add-reservations/'
                     )
 
         time.sleep(1)
-        # Input the name of the resort
 
-        browser.find_element_by_class_name('sc-pAXKH').send_keys(
-                                                    'crystal mountain'
-                                                                )
+        # Input the name of the resort
+        browser.find_element_by_class_name('sc-pAXKH').send_keys(self.resort)
+
         time.sleep(1)
 
-        # Select the first choice that comes up
-        browser.find_element_by_xpath('//*[@id="react-autowhatever-resort-picker-section-0-item-0"]').click()
+        # Select the first choice in the list that comes up
+        browser.find_element_by_xpath(
+        '//*[@id="react-autowhatever-resort-picker-section-0-item-0"]'
+                                    ).click()
 
         # Click continue to get to the calendar
-        browser.find_element_by_class_name('sc-AxjAm.jxPclZ.sc-prpXb.hoYObS').click()
+        browser.find_element_by_class_name(
+                            'sc-AxjAm.jxPclZ.sc-prpXb.hoYObS'
+                                            ).click()
+
+        # If the month of reservation is not the current month,
+        # toggle calendar to the correct month
+        month_year = self.date.strftime('%B %Y').upper()
+
+        # Check what month we are looking at
+        while month_year != browser.find_element_by_class_name('sc-pZMVu.hgRLdf').text:
+
+        # Add one month to the calendar if we're not looking at the correct one
+            browser.find_element_by_class_name('amp-icon.icon-chevron-right').click()
+
+        # The dates are stored in the format 'Thu Dec 10 2020'
+        # Let's convert our desired reservation date into that format
+
+        calendar_date = self.date.strftime('%a %b %d %Y')
+
+        # Click on the desired calendar date
+        browser.find_element_by_css_selector(
+                        "[aria-label=" + calendar_date + "]"
+                                            ).click()
+
+        # See if the button to reserve is there, if not, continue.
+        slot_found = False
+        try:
+            browser.find_element_by_class_name(
+                        'sc-AxjAm.jxPclZ.sc-qPNpY.fZKxnA'
+                                            ).click()
+            slot_found = True
+
+        except NoSuchElementException:
+            pass
+
+        if slot_found:
+            return browser
 
 
-    #
-    #     # If the month of reservation is not the current month,
-    #     # toggle calendar to the following month (assuming you won't book more
-    #     # than 29 days in advance)
-    #     if self.month > datetime.now().month:
-    #         browser.find_element_by_xpath(
-    #             '/html/body/div[1]/form/div[6]/fieldset/div/div/div/a[2]/span'
-    #                                      ).click()
-    #
-    #     # Select the day of interest
-    #     browser.find_element_by_xpath("//td[not(contains(@class,'ui-datepicker-month'))]/a[text()='" + str(self.day) + "']").click()
-    #
-    #     # Add one participant to the booking
-    #     browser.find_element_by_xpath('/html/body/div[1]/form/div[6]/div/fieldset/table/tbody/tr/td[1]/a[2]').click()
-    #
-    #     # Selected is there to check if we've been able to select the slot
-    #     # of interest (to know when to exit browser or return it at the end
-    #     # of function)
-    #     selected = False
-    #
-    #     # Iterate through the table containing all the slots for the day
-    #     for i in range(1, 17):
-    #         # This is the location of the table row containing the time of
-    #         # reservation
-    #         row_xpath = '/html/body/div[1]/form/fieldset/div[1]/table/tbody/tr[' + str(i) + ']/td[1]'
-    #
-    #         try:
-    #             # Format of temp_slot is the following:
-    #             # "Tue, September 29, 7 AM to 9 AM"
-    #             temp_slot = browser.find_element_by_xpath(row_xpath).text
-    #             temp_time = temp_slot.split(',')[-1]
-    #             start_time = temp_time.split('to')[0].strip()
-    #
-    #             # If this is the slot desired, let's book it!
-    #             # Check that the slot we're looking at starts at the time
-    #             # that was specified
-    #             if self.hour == start_time:
-    #
-    #                 # Click the button adjacent to that slot if it exists
-    #                 # If not, caught by exception and quit browser.
-    #                 button_xpath = '/html/body/div[1]/form/fieldset/div[1]/table/tbody/tr[' + str(i) + ']/td[4]/a'
-    #                 browser.find_element_by_xpath(button_xpath).click()
-    #
-    #                 print('Found the reservation you were looking for!')
-    #                 selected = True
-    #                 break
-    #
-    #         except Exception as ex:
-    #             print('Caught an exception:', ex)
-    #             print("\n")
-    #             continue
-    #
-    #     # If the slot is available and selected, we can then return the browser
-    #     # at the end of function so that we can go on to book it.
-    #     if selected:
-    #         return browser
-    #
-    #     browser.quit()
-    #
-    # def book_slot(self, browser, test_mode=True):
-    #     """
-    #     When given a browser where the user is identified and the desired
-    #     slot has been selected, this function will book the slot.
-    #
-    #     Test mode means that everything is filled out but the submit button
-    #     isn't clicked.
-    #     """
-    #
-    #     # This global variable lets the scheduler know when to stop looking
-    #     # for the desired slot (i.e. when the slot has been booked)
-    #     # global is_reserved
-    #
-    #     try:
-    #         # Click on the profile name to autofill the form
-    #         browser.find_element_by_xpath('/html/body/div[1]/form/fieldset[1]/div/div[1]/div/a').click()
-    #
-    #         # Give the browser a little time to autofill the form
-    #         # Otherwise results in crash
-    #         time.sleep(1)
-    #
-    #         # Confirm that all waivers have been signed
-    #         browser.find_element_by_xpath('/html/body/div[1]/form/fieldset[2]/div[2]/select/option[2]').click()
-    #         browser.find_element_by_xpath('/html/body/div[1]/form/fieldset[3]/div[2]/select/option[2]').click()
-    #
-    #         # Click continue
-    #         browser.find_element_by_xpath('/html/body/div[1]/form/a[2]').click()
-    #
-    #         # Agree to the T&Cs
-    #         browser.find_element_by_xpath('/html/body/div[1]/form/fieldset[5]/div[2]/input').click()
-    #
-    #         # Submit form if not in test mode, i.e. actually want to book the
-    #         # slot
-    #         if not test_mode:
-    #             browser.find_element_by_xpath(
-    #                     '//*[@id="confirm_booking_button"]').click()
-    #
-    #         # If we got to submit the form, it's that the slot was booked
-    #         # Need to change is_reserved to True, so that the scheduler knows
-    #         # to stop
-    #         # is_reserved = True
-    #         print('We booked the slot you were after!')
-    #         time.sleep(1)
-    #
-    #         # If in test_mode, want to leave the browser open so can inspect
-    #         # that everything has been done correctly.
-    #         if not test_mode:
-    #             browser.quit()
-    #
-    #         return schedule.CancelJob
-    #
-    #     except Exception as ex:
-    #         print('Found desired slot but could not book slot because of error after slot selection.')
-    #         print('Exception:', ex)
+    def book_slot(self, browser, test_mode=True):
+        """
+        This function is called only if find_slot() returns the browser instance
+        that has found the slot successfully.
+        """
+
+        # Click on review my reservations button
+        browser.find_element_by_class_name(
+                            'sc-AxjAm.jxPclZ.sc-qOubn.cugtRd'
+                                        ).click()
+
+        # Tick the checkbox
+        browser.find_element_by_class_name('input').click()
+
+        # Submit the form
+        # If in test_mode, want to leave the browser open so can inspect that
+        # everything has been done correctly.
+
+        if not test_mode:
+            browser.find_element_by_class_name('sc-AxjAm.jxPclZ').click()
+            print('Found the desired slot, for date:', self.date)
+            browser.quit()
+
+        # If we booked the desired slot we can stop the scheduler from
+        # scheduling that job again
+        return schedule.CancelJob
 
 
 # for booking_number in credentials.booking:
@@ -266,19 +213,22 @@ for booking_number in credentials.booking:
 #         print("exception caught in the scheduler loop: ", ex)
 
 
-browser = webdriver.Firefox(executable_path='/Users/tchavas/geckodriver')
+# browser = webdriver.Firefox(executable_path='/Users/tchavas/geckodriver')
+browser = webdriver.Chrome(executable_path='/Users/tchavas/chromedriver')
+# browser.get('https://account.ikonpass.com/en/myaccount/add-reservations/')
+browser.get('https://account.ikonpass.com/en/login')
 
-browser.get('https://account.ikonpass.com/en/myaccount/add-reservations/')
-
+time.sleep(1)
 # # Fill in your details
-browser.find_element_by_xpath('//*[@id="email"]').send_keys('thomatou@hotmail.com')
-browser.find_element_by_xpath('//*[@id="sign-in-password"]').send_keys('FhF8htNLnaAeQUq')
+browser.find_element_by_xpath('//*[@id="email"]').send_keys(credentials.booking[0]['email'])
+browser.find_element_by_xpath('//*[@id="sign-in-password"]').send_keys(credentials.booking[0]['password'])
 
 time.sleep(1)
 
 # Submit your login info
 browser.find_element_by_class_name('submit').click()
 
+time.sleep(1)
 
 browser.get('https://account.ikonpass.com/en/myaccount/add-reservations/')
 
@@ -296,13 +246,25 @@ browser.find_element_by_class_name('sc-AxjAm.jxPclZ.sc-prpXb.hoYObS').click()
 
 
 # select a particular date – first have to toggle to correct month
-browser.find_element_by_css_selector("[aria-label='Thu Nov 19 2020']").click()
+browser.find_element_by_css_selector("[aria-label='Wed Nov 25 2020']").click()
 
+# Check the month you're looking at
+browser.find_element_by_class_name('sc-pZMVu.hgRLdf').text
 
+# Click on the reservation button
+browser.find_element_by_class_name('sc-AxjAm.jxPclZ.sc-qPNpY.fZKxnA').click()
 
+# # add one month to calendar
+# browser.find_element_by_class_name('amp-icon.icon-chevron-right').click()
+#
+# # select a particular date – first have to toggle to correct month
+# browser.find_element_by_css_selector("[aria-label='Thu Dec 10 2020']").click()
 
-# add one month to calendar
-browser.find_element_by_class_name('amp-icon.icon-chevron-right').click()
+# Click on review my reservations button
+browser.find_element_by_class_name('sc-AxjAm.jxPclZ.sc-qOubn.cugtRd').click()
 
-# select a particular date – first have to toggle to correct month
-browser.find_element_by_css_selector("[aria-label='Thu Dec 10 2020']").click()
+# Tick the checkbox
+browser.find_element_by_class_name('input').click()
+
+# Submit
+browser.find_element_by_class_name('sc-AxjAm.jxPclZ').click()
