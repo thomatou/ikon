a2291dd29ae64fd93e9858bf80237f7df5b72ba1
thomatou <tc408@ic.ac.uk>
2020-11-29 22:05:55 -0800

    corrected the format of the mock_credentials.py file

--- mock_credentials.py
+++ mock_credentials.py
@@ -13,4 +13,13 @@ booking =   {
 'password' : 'your_password',
 'resort' : 'Crystal Mountain Resort',
 }
-
+,
+0: {
+'month': 12,
+'day' : 21,
+'email' : 'another_email',
+'password' : 'another_password',
+'resort' : 'Arapahoe Basin',
+}
+,
+            }
